ec7982a9d4fbd89d5a64d60af0f906078fea41f6
Abdul Mannan <abdul.mannan@mavs.uta.edu>
2019-03-23 23:25:54 -0500

    color selector is added, it selects color in same activity but does not update colors in mainactivity

---
+++ app/src/main/java/com/example/abkire/project3/ChangeColorsActivity.java
@@ -0,0 +1,54 @@
+package com.example.abkire.project3;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class ChangeColorsActivity extends AppCompatActivity {
+
+    Button postClickBtn;
+    int postClickBtnColor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_colors);
+
+        // get the intent that started this activity and extract data
+        Intent intent = getIntent();
+
+        postClickBtn = (Button)findViewById(R.id.postClickBtn);
+        postClickBtnColor = ContextCompat.getColor(ChangeColorsActivity.this, R.color.postclick);
+
+        postClickBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                openColorPicker();
+            }
+        });
+    }
+
+    private void openColorPicker() {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, postClickBtnColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+                // do nothing.
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                postClickBtnColor = color;
+                postClickBtn.setBackgroundColor(postClickBtnColor);
+            }
+        });
+        colorPicker.show();
+    }
+}
--- app/src/main/java/com/example/abkire/project3/MainActivity.java
+++ app/src/main/java/com/example/abkire/project3/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.abkire.project3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Integer> viewIdList = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void  setColors(View view){
+        // Intent to start new activity
+        Intent intent = new Intent(MainActivity.this, ChangeColorsActivity.class);
+        MainActivity.this.startActivity(intent);
+    }
+
+    public void changeCardColor(View view) {
+        int viewid = view.getId();
+        CardView card = view.findViewById(viewid);
+        card.setCardBackgroundColor(getResources().getColor(R.color.postclick));
+        if (!viewIdList.contains(viewid)) {
+            viewIdList.add(viewid);
+        }
+        if(viewIdList.size() ==15){
+            Toast.makeText(getApplicationContext(), "You Win! ", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
