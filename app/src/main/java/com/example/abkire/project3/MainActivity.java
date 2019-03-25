0299f00a4ce2909a85901b29fabb54a8b0e89cde
abkire <erikandrup@gmail.com>
2019-03-25 11:46:42 -0500

    Added sounds and reset button - still to implement reset

--- app/src/main/java/com/example/abkire/project3/MainActivity.java
+++ app/src/main/java/com/example/abkire/project3/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.abkire.project3;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> viewIdList = new ArrayList<Integer>();
+    private Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        resetButton = (Button) findViewById(R.id.reset_button);
+
+
+        resetButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                resetCards();
+
+
+              }
+            });
+
+        }
 
     public void  setColors(View view){
         // Intent to start new activity
@@ -34,7 +52,15 @@ public class MainActivity extends AppCompatActivity {
         }
         if(viewIdList.size() ==15){
             Toast.makeText(getApplicationContext(), "You Win! ", Toast.LENGTH_LONG).show();
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.pop);
+            mp.start();
         }
+
+    }
+    public void resetCards()
+    {
+        Toast.makeText(getApplicationContext(), "Reset Cards", Toast.LENGTH_LONG).show();
+        int viewid =
 
     }
 }
